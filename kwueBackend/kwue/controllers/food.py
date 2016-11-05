215fd0a6846dc3ca1b4d0b731a477b7b5edf41c6
A. Seleme Topuz <seleme94@hotmail.com>
2016-11-05 20:10:07 +0200

    request variable can now be read as an HttpReqeust

--- kwueBackend/kwue/controllers/food.py
+++ kwueBackend/kwue/controllers/food.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 from kwue.DB_functions.food_db_functions import *
 
-def get_food(req):
-    print(db_retrieve_food(1).__dict__)
+def get_food(req, food_id):
+    print(db_retrieve_food(food_id).__dict__)
+
     return render(req, 'kwue/food.html', {})
 
 def add_food(req):
@@ -14,7 +17,7 @@ def remove_food(req):
     return render(req, 'kwue/food.html', {})
 
 def rate_food(req):
-    return render(req, 'kwue/food.html', {})
+    return render(req, 'kwue/home.html', {})
 
 def comment_food(req):
     return render(req, 'kwue/food.html', {})
--- kwueBackend/kwue/urls.py
+++ kwueBackend/kwue/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from kwue.controllers import food
 
 urlpatterns = (
-    url(r'^get_a_food/', food.get_food),
-    url(r'^home/', food.add_food),
+    url(r'^get_a_food/(?P<food_id>[0-9]+)', food.get_food),
+    url(r'^home/', food.rate_food),
+    url(r'^', food.rate_food),
 )

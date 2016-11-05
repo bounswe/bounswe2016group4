932c3ac96a4ac08a73c2ac3b5b065dd53f3ae479
alptekinorbay <alptekin.orbay@gmail.com>
2016-11-05 17:29:37 +0300

    DB file modified

--- kwueBackend/kwue/admin.py
+++ kwueBackend/kwue/admin.py
@@ -3,9 +3,10 @@ from django.contrib import admin
 # Register your models here.
 
 from django.contrib import admin
-from .models.models import *
+from kwue.models.models import *
 
 admin.site.register(UserModel)
 admin.site.register(FoodModel)
 admin.site.register(CommentModel)
 admin.site.register(ListModel)
+admin.site.register(ConsumptionHistory)

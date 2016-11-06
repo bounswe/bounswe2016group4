13eef238d80c920ab95b09bf5203a26c8bffafa0
Alper Ahmetoglu <ahmetoglu.alper@gmail.com>
2016-11-06 03:53:42 +0200

    add-food view implemented

--- kwueBackend/kwue/controllers/food.py
+++ kwueBackend/kwue/controllers/food.py
@@ -6,6 +6,9 @@ def get_food(req, food_id):
     return render(req, 'kwue/food.html', {})
 
 def add_food(req):
+
+    ## if'li kısmın değişmesi gerekiyor bir işe yaramıyor şu an
+    ## isterseniz debug için tutun
     if req.method == 'GET':
         print("get methodu")
         getData = req.GET.dict()
@@ -13,6 +16,14 @@ def add_food(req):
             searchValuesu = getData['search']
             print(searchValuesu)
             return render(req, 'kwue/add_food.html', {'s': searchValuesu})
+    elif req.method == 'POST':
+        data = req.POST.dict()
+        ## food'u database'e ekleme kısmı...
+        ##
+        ##
+        ## ekledikten sonra render edilecek sayfa.
+        return render(req, 'kwue/home.html', {})
+
     return render(req, 'kwue/add_food.html', {})
 
 def remove_food(req,food_id):

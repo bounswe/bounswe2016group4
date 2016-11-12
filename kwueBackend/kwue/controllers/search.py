119a9248c119a4dd1bd34c177d032bef109025a5
A. Seleme Topuz <seleme94@hotmail.com>
2016-11-12 11:28:36 +0300

    comments added about how the search user and consumption history features will be done

--- kwueBackend/kwue/controllers/search.py
+++ kwueBackend/kwue/controllers/search.py
@@ -3,9 +3,15 @@ from django.shortcuts import render
 def get_search_info(req):
     return render(req, 'kwue/food.html', {})
 
-def search(req):
+def basic_search(req):
+    # 1 - get user eating preferences from db with session
+    # 2 - get foods and food servers with same text (or the text is in the food/server) considering eating preferences)
     return render(req, 'kwue/food.html', {})
 
-def adv_search(req):
+def advanced_search(req):
+    # 1 - check if the user checkboxed the "ignore my eating preferences"
+    # 2 - get user eating preferences from db with session
+    # 3 - get all filters
+    # 4 - get foods and food servers with the same text (or the text is in the food/server) considering filters)
     return render(req, 'kwue/food.html', {})
 
--- kwueBackend/kwue/controllers/user.py
+++ kwueBackend/kwue/controllers/user.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 
 def get_user(req):
+    # 1 - Get user from session from db
     return render(req, 'kwue/food.html', {})
 
 def update_profile(req):
     return render(req, 'kwue/food.html', {})
 
 def get_consumption_history(req):
+    # 1 - From the tabs "last one day" "last one week" "last one month" "all time" the last one day is default.
+    # 2 - Get the foods eaten last one day with the taken nutrition values from db.
     return render(req, 'kwue/food.html', {})

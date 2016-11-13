d40b830e27bcbce867eb3348ab9b60e404cb919e
alptekinorbay <alptekin.orbay@gmail.com>
2016-11-14 00:25:13 +0300

    Rate Searc Function Implemented

--- kwueBackend/kwue/DB_functions/search_db_function.py
+++ kwueBackend/kwue/DB_functions/search_db_function.py
@@ -9,30 +9,35 @@ from kwue.models.models import FoodModel
 def allergic_search(ingredient_list, foods=None):
     if foods is None:
         foods = FoodModel.objects.all()
-    return list(foods.exclude(ingredient_list__in=ingredient_list))
+    return foods.exclude(ingredient_list__in=ingredient_list)
 
 
 def protein_search(lower_bound=0, upper_bound=1000, foods=None):
     if foods is None:
         foods = FoodModel.objects.all()
-    return list(foods.filter(protein_value__range=(lower_bound, upper_bound)))
+    return foods.filter(protein_value__range=(lower_bound, upper_bound))
 
 
-def fat_search(lower_bound=0,upper_bound=1000, foods=None):
+def fat_search(lower_bound=0, upper_bound=1000, foods=None):
     if foods is None:
         foods = FoodModel.objects.all()
-    return list(foods.filter(fat_value__range=(lower_bound, upper_bound)))
+    return foods.filter(fat_value__range=(lower_bound, upper_bound))
 
 
-def carbohydrate_search(lower_bound=0,upper_bound=1000, foods=None):
+def carbohydrate_search(lower_bound=0, upper_bound=1000, foods=None):
     if foods is None:
         foods = FoodModel.objects.all()
-    return list(foods.filter(carbohydrate_value__range=(lower_bound, upper_bound)))
+    return foods.filter(carbohydrate_value__range=(lower_bound, upper_bound))
 
 
-def calorie_search(lower_bound=0,upper_bound=1000, foods=None):
+def calorie_search(lower_bound=0, upper_bound=1000, foods=None):
     if foods is None:
         foods = FoodModel.objects.all()
-    return list(foods.filter(calorie_value__range=(lower_bound, upper_bound)))
+    return foods.filter(calorie_value__range=(lower_bound, upper_bound))
 
+
+def rate_search(lower_bound=0, upper_bound=5, foods=None):
+    if foods is None:
+        foods = FoodModel.objects.all()
+    return foods.filter(food_rate__range=(lower_bound, upper_bound))
 

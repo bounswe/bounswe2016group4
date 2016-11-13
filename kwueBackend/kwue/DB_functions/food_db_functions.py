90f518cbb806a3acde9b73e79bc2dd4b096d6589
alptekinorbay <alptekin.orbay@gmail.com>
2016-11-13 19:25:52 +0300

    Micronutrients Calculation and Insert Implemented

--- kwueBackend/kwue/DB_functions/food_db_functions.py
+++ kwueBackend/kwue/DB_functions/food_db_functions.py
@@ -3,14 +3,12 @@ from kwue.DB_functions.user_db_function import db_retrieve_user
 
 
 def db_insert_food(food_dict, nutrition_dict):
-
     food_owner_id = food_dict['food_owner']
     food_owner = db_retrieve_user(food_owner_id)
     new_food = FoodModel(
         food_description=food_dict['food_description'],
         food_name=food_dict['food_name'],
         food_image=food_dict['food_image'],
-        #food_owner=food_dict['food_owner'],
         food_owner=food_owner,
         food_recipe=food_dict['food_recipe'],
         protein_value=nutrition_dict['protein_value'],
@@ -20,7 +18,34 @@ def db_insert_food(food_dict, nutrition_dict):
         calorie_value=nutrition_dict['calorie_value'],
         sugar_value=nutrition_dict['sugar_value'],
         serving_weight_grams=nutrition_dict['serving_weight_grams'],
-
+        vitamin_A=nutrition_dict['vitamin_A'],
+        vitamin_C=nutrition_dict['vitamin_C'],
+        vitamin_D=nutrition_dict['vitamin_D'],
+        vitamin_E=nutrition_dict['vitamin_E'],
+        vitamin_K=nutrition_dict['vitamin_K'],
+        thiamin=nutrition_dict['thiamin'],
+        riboflavin=nutrition_dict['riboflavin'],
+        niacin=nutrition_dict['niacin'],
+        vitamin_B6=nutrition_dict['vitamin_B6'],
+        folatem=nutrition_dict['folatem'],
+        vitamin_B12=nutrition_dict['vitamin_B12'],
+        pantothenic_acid=nutrition_dict['pantothenic_acid'],
+        biotin=nutrition_dict['biotin'],
+        choline=nutrition_dict['choline'],
+        calcium=nutrition_dict['calcium'],
+        chromium=nutrition_dict['chromium'],
+        copper=nutrition_dict['copper'],
+        clouride=nutrition_dict['clouride'],
+        flouride=nutrition_dict['flouride'],
+        iodine=nutrition_dict['iodine'],
+        iron_Fe=nutrition_dict['iron_Fe'],
+        magnesium=nutrition_dict['magnesium'],
+        manganese=nutrition_dict['manganese'],
+        molybdenum=nutrition_dict['molybdenum'],
+        sodium_Na=nutrition_dict['sodium_Na'],
+        phosphorus=nutrition_dict['phosphorus'],
+        selenium=nutrition_dict['selenium'],
+        zinc=nutrition_dict['zinc']
     )
     try:
         new_food.save()
@@ -32,6 +57,7 @@ def db_insert_food(food_dict, nutrition_dict):
 def db_retrieve_food(food_id):
     food = FoodModel.objects.get(food_id=food_id)
     return food
+
 
 ## to be deleted later if unused
 def db_retrieve_all_foods():
@@ -90,5 +116,3 @@ def db_add_consumption_history(user_id, food_id, rate=0):
         return True
     except:
         return False
-
-
--- kwueBackend/kwue/helper_functions/nutrition_helpers.py
+++ kwueBackend/kwue/helper_functions/nutrition_helpers.py
@@ -4,24 +4,69 @@ import requests
 
 def parse_nutrition(data):
     data = json.loads(data)
+    full_nutrients_dict = parse_full_nutrients(data)
     nutrition_dict = dict(protein_value=data['foods'][0]['nf_protein'], fat_value=data['foods'][0]['nf_total_fat'],
                           carbohydrate_value=data['foods'][0]['nf_total_carbohydrate'],
                           fiber_value=data['foods'][0]['nf_dietary_fiber'],
                           calorie_value=data['foods'][0]['nf_calories'], sugar_value=data['foods'][0]['nf_sugars'],
                           serving_weight_grams=data['foods'][0]['serving_weight_grams'])
+    for key, value in full_nutrients_dict.items():
+        nutrition_dict[key] = value
+    print(nutrition_dict)
     return nutrition_dict
 
 
-def request_nutrition(recipe):
+def parse_full_nutrients(data):
+    full_nutr_dict = dict(
+        vitamin_A=318,
+        vitamin_C=401,
+        vitamin_D=328,
+        vitamin_E=573,
+        vitamin_K=430,
+        thiamin=404,
+        riboflavin=405,
+        niacin=406,
+        vitamin_B6=415,
+        folatem=432,
+        vitamin_B12=418,
+        pantothenic_acid=410,
+        biotin=410,  ###################
+        choline=421,
+        calcium=301,
+        chromium=301,  ####################
+        copper=312,
+        clouride=312,  ############
+        flouride=313,
+        iodine=313,  ##################
+        iron_Fe=303,
+        magnesium=304,
+        manganese=315,
+        molybdenum=315,  ##############
+        sodium_Na=307,
+        phosphorus=305,
+        selenium=317,
+        zinc=309
+    )
+    for entry in data['foods'][0]["full_nutrients"]:
+            nutrient_id = entry['attr_id']
+            value = entry['value']
+            for key, number in full_nutr_dict.items():
+                if nutrient_id == number:
+                    full_nutr_dict[key] = value
+    return full_nutr_dict
+
+
+def request_nutrition(recipe, num_servings=1):
     url = 'https://trackapi.nutritionix.com/v2/natural/nutrients'
     app_id = '24f13571'
     app_key = '0acc5c00e3aa18a297a3455338ffac28'
     headers = {'Content-Type': 'application/json', 'x-app-id': app_id, 'x-app-key': app_key}
-    sent_data = json.dumps({'query':recipe, "aggregate": "recipe"})
+    sent_data = json.dumps(
+        {'query': recipe, "aggregate": "recipe", "line_delimited": True, "num_servings": num_servings})
     r = requests.post(url=url, headers=headers, data=sent_data)
+    print(r.text)
     if r.status_code == 200:
         return parse_nutrition(r.text)
     else:
         print('requests cannot handle', r.status_code)
         return None
-

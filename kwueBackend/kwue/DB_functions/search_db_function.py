32906ce1e9d5374b3b9e0c7c3e936ae4cf8e2191
alptekinorbay <alptekin.orbay@gmail.com>
2016-11-14 00:12:21 +0300

    Basic Nutrition Value Search, Allergic Search Implemented

--- kwueBackend/kwue/DB_functions/search_db_function.py
+++ kwueBackend/kwue/DB_functions/search_db_function.py
@@ -11,3 +11,28 @@ def allergic_search(ingredient_list, foods=None):
         foods = FoodModel.objects.all()
     return list(foods.exclude(ingredient_list__in=ingredient_list))
 
+
+def protein_search(lower_bound=0,upper_bound=1000,foods=None):
+    if foods is None:
+        foods = FoodModel.objects.all()
+    return list(foods.filer(protein_value__range=(lower_bound,upper_bound)))
+
+
+def fat_search(lower_bound=0,upper_bound=1000,foods=None):
+    if foods is None:
+        foods = FoodModel.objects.all()
+    return list(foods.filer(fat_value__range=(lower_bound,upper_bound)))
+
+
+def carbohydrate_search(lower_bound=0,upper_bound=1000,foods=None):
+    if foods is None:
+        foods = FoodModel.objects.all()
+    return list(foods.filer(carbohydrate_value__range=(lower_bound,upper_bound)))
+
+
+def calorie_search(lower_bound=0,upper_bound=1000,foods=None):
+    if foods is None:
+        foods = FoodModel.objects.all()
+    return list(foods.filer(calorie_value__range=(lower_bound,upper_bound)))
+
+

7f58f1cbbae652da6e868dc9311c03098b5bdd8f
alptekinorbay <alptekin.orbay@gmail.com>
2016-11-16 22:35:08 +0300

    Basic Search Implemented

--- kwueBackend/kwue/DB_functions/search_db_function.py
+++ kwueBackend/kwue/DB_functions/search_db_function.py
@@ -1,9 +1,26 @@
-from kwue.models.models import FoodModel
+from kwue.models.models import FoodModel, TagModel, UserModel
+from django.contrib.postgres.search import TrigramSimilarity
 
 
 # def allergic_excluder(foods, ingredient):
 #     foods.exclude(ingredient_list__in=[ingredient])
 #     return foods
+
+
+def basic_search(focus_string):
+    similar_foods = FoodModel.objects.filter(food_name__icontains=focus_string)
+    from_tag = TagModel.objects.filter(tag_label__icontains=focus_string) | TagModel.objects.filter(
+        semantic_tag_item_label__icontains=focus_string)
+    from_tag_food = from_tag.filter(content_type__model="FoodModel")
+    foods = FoodModel.objects.filter(food_id__in=from_tag_food.values('tagged_object_id')) | similar_foods
+    similar_food_servers = UserModel.objects.filter(user_nick__icontains=focus_string)
+    from_tag_food_server = from_tag.filter(content_type__model='UserModel').distinct()
+    food_servers = UserModel.objects.filter(user_id__in=from_tag_food_server.values('tagged_object_id')) | similar_food_servers
+    search_dict = dict(
+        food=foods,
+        food_server=food_servers
+    )
+    return search_dict
 
 
 def unwanted_search(ingredient_list, foods=None):
@@ -46,4 +63,3 @@ def rate_search(lower_bound=0, upper_bound=5, foods=None):
     if foods is None:
         foods = FoodModel.objects.all()
     return foods.filter(food_rate__range=(lower_bound, upper_bound))
-

eebbab53148a7fbb1ec244cc2bc1d3b40457e074
alptekinorbay <alptekin.orbay@gmail.com>
2016-11-16 23:25:40 +0300

    Consumption History add,retrieve implemented at DB

---
+++ kwueBackend/kwue/DB_functions/consumption_history_db_functions.py
@@ -0,0 +1,29 @@
+from kwue.models.models import ConsumptionHistory,FoodModel
+from kwue.DB_functions.food_db_functions import db_retrieve_food
+from kwue.DB_functions.user_db_function import db_retrieve_user
+
+
+def db_insert_consumption_record(user_id,food_id):
+    try:
+        user = db_retrieve_user(user_id)
+        food = db_retrieve_food(food_id)
+        record = ConsumptionHistory(
+            food=food,
+            user=user
+        )
+        record.save()
+        return record
+    except:
+        return False
+
+
+def db_search_consumption_records(start_timestamp_date,end_timestamp_date,user_id):
+    try:
+        user = db_retrieve_user(user_id)
+        records = ConsumptionHistory.objects.filter(date__range=(start_timestamp_date,end_timestamp_date), user=user)
+        foods = records.values('food')
+        dates = records.values('date')
+        return {'food': foods, 'date': dates}
+
+    except:
+        return False

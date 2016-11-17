4c0a5cab060c1dd098a44cf931bb5dd91e02bcdd
alptekinorbay <alptekin.orbay@gmail.com>
2016-11-17 04:07:18 +0300

    Initial Semantic Search Integration to Basic Search

--- kwueBackend/kwue/DB_functions/search_db_function.py
+++ kwueBackend/kwue/DB_functions/search_db_function.py
@@ -1,29 +1,42 @@
 from kwue.models.models import FoodModel, TagModel, UserModel
 from django.contrib.postgres.search import TrigramSimilarity
-
-
+from kwue.helper_functions.semantic_tag_helpers import most_common
+from collections import Counter
 # def allergic_excluder(foods, ingredient):
 #     foods.exclude(ingredient_list__in=[ingredient])
 #     return foods
 
 
 def basic_search(focus_string):
+    semantic_relation_food = FoodModel.objects.filter(food_name__iexact=focus_string).values_list('food_id')
+    semantic_tags = list(TagModel.objects.filter(tagged_object_id__in=semantic_relation_food,content_type__model="foodmodel").values_list('semantic_tag_item_description'))
+
+    semantic_relation_user = UserModel.objects.filter(user_name__iexact=focus_string).values_list('user_id')
+    semantic_tags.extend(list(TagModel.objects.filter(tagged_object_id__in=semantic_relation_user,content_type__model="usermodel").values_list('semantic_tag_item_description')))
+
+    print(semantic_tags)
+    mcommon = [ite[0] for ite, it in Counter(semantic_tags).most_common(5)]
+    semantic_objects = TagModel.objects.filter(semantic_tag_item_description__in=mcommon)
+    semantic_food = []
+    semantic_user = []
+    print(semantic_objects)
+    for x in list(semantic_objects):
+        if x.content_type.model == "foodmodel":
+            semantic_food.append(x.tagged_object)
+        else:
+            semantic_user.append(x.tagged_object)
     similar_foods = FoodModel.objects.filter(food_name__icontains=focus_string)
     from_tag = TagModel.objects.filter(tag_label__icontains=focus_string) | TagModel.objects.filter(
         semantic_tag_item_label__icontains=focus_string)
     print(from_tag)
     from_tag_food = from_tag.filter(content_type__model="foodmodel")
-    print(list(from_tag)[0].content_type.model)#[0].content_type)
     foods = FoodModel.objects.filter(food_id__in=from_tag_food.values('tagged_object_id')) | similar_foods
     similar_food_servers = UserModel.objects.filter(user_name__icontains=focus_string)
     print(similar_food_servers)
     from_tag_food_server = from_tag.filter(content_type__model='usermodel')
     food_servers = UserModel.objects.filter(user_id__in=from_tag_food_server.values('tagged_object_id')) | similar_food_servers
-    search_dict = dict(
-        food=list(foods),
-        food_server=list(food_servers)
-    )
-    return search_dict
+    return dict(food_set=foods, food_server_set=food_servers,semantic_foods=semantic_food,semantic_users=semantic_user)
+
 
 
 def unwanted_search(ingredient_list, foods=None):

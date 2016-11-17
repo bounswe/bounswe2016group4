a18c992c1ed2b2ef07aec2843b247ff44bf6cdc6
Doruk <dorukdemirci.boun@gmail.com>
2016-11-17 03:04:18 +0300

    sign_up and log_in functions was implemented.

--- kwueBackend/kwue/controllers/user.py
+++ kwueBackend/kwue/controllers/user.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from kwue.DB_functions.food_db_functions import *
-
+from kwue.DB_functions.user_db_function import *
 def get_user(req):
     # 1 - Get user from session from db
     return render(req, 'kwue/food.html', {})
@@ -22,9 +22,35 @@ def get_consumption_history(req):
 
 def sign_up(req):
     #DB ye user kaydedilsin
+
+    new_user_dict = req.POST.dict()
+
+    #Check that the email adress has not been used before !!!!!!!
+    user_email_address = new_user_dict['user_email_address']
+
+    user_information_dict = dict(
+        user_name=new_user_dict["user_name"],
+        user_nick=new_user_dict["user_nick"],
+        user_email_address=new_user_dict["user_email_address"],
+        user_password=new_user_dict["user_password"],
+        user_image=new_user_dict["user_image"],
+        user_type=new_user_dict["user_type"]
+    )
+    db_insert_user(user_information_dict)
     return render(req, 'kwue/food.html', {})
 
 def login(req):
     # DB den user ın var olup olmadığına dair bilgi gelsin
     # DB bana userın id sini versin ve o id ile session başlasın
+
+    user_dict = req.POST.dict()
+    user_email_address = user_dict['user_email_address']
+    user_password = user_dict["user_password"]
+
+    if db_validate_user(user_email_address, user_password):
+        print("User"+user_email_address+ "exists")
+        req.session['email_adress']= user_email_address #SESSION STARTS
+    else:
+        print("User does not exists.")
+
     return render(req, 'kwue/food.html', {})

3af72491c50b7ef91cf38d4df7fa232656ca363b
Alper Ahmetoglu <ahmetoglu.alper@gmail.com>
2016-11-20 11:50:30 +0300

    add food page updated, there is a problem with adding post request data

--- kwueBackend/kwue/static/js/food.js
+++ kwueBackend/kwue/static/js/food.js
@@ -6,6 +6,18 @@ $(document).ready(function(){
         $("#555").hide();
     });
 
+    $("#submit-button").click(function () {
+        var data = $('#food-form').serializeArray();
+        var ingredients = [];
+        $(".ing-group").each(function () {
+            var name = $(this).children("#ingredient-1").val();
+            var value = $(this).children("#ingredient-1-val").val();
+            var temp = {'ingredient': name, 'value': value};
+        });
+        data.push({name: 'ingredients', value: ingredients});
+        $.post("add_food", data);
+    });
+
     $("#tag_button").click(function () {
         var tag_name = $("#sem-tags").val();
         $.ajax({
@@ -32,13 +44,13 @@ $(document).ready(function(){
         var html = "<div class='form-group ing-group'>" +
             "<label for='ingredient-1'>Ingredient 1</label>" +
             "<a href='#' class='btn btn-danger ingredient-delete' id='ingredient-delete'><span class='glyphicon glyphicon-minus-sign'></span></a>" +
-            "<input type='text' class='form-control' id='ingredient-1' value=''>" +
-            "<input type='number' class='form-control' id='ingredient-1-val' value=''>" +
-            "</div>"
+            "<input type='text' class='form-control ing-name' id='ingredient-1' value='' placeholder='Ingredient name'>" +
+            "<input type='number' class='form-control ing-gram' id='ingredient-1-val' value='' placeholder='Amount in grams' min='1'>" +
+            "</div>";
         $("#ingredient-add").before(html);
     })
 });
 
 $(document).on('click', '.ingredient-delete', function () {
     $(this).parent().remove();
-})
+});
